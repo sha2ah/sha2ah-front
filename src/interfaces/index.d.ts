e2e8e34c67a106b57f68d62c4b5ff22b5e3fa92a
akilany <ammyass2@gmail.com>
2022-02-27 19:30:13 +0200

    feat: added contract interface

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,50 +1,59 @@
 export interface ICategory {
-  id: string;
-  title: string;
+  id: string
+  title: string
 }
 export interface IPost {
-  id: string;
-  title: string;
-  content: string;
-  status: "published" | "draft" | "rejected";
-  createdAt: string;
-  category: ICategory;
+  id: string
+  title: string
+  content: string
+  status: 'published' | 'draft' | 'rejected'
+  createdAt: string
+  category: ICategory
 }
 
 export interface IRenter {
-  id: string;
-  name: string;
-  phone: string;
-  status: string;
-  email?: string;
-  details?: string;
-  createdAt: string;
+  id: string
+  name: string
+  phone: string
+  status: string
+  email?: string
+  details?: string
+  createdAt: string
 }
 
 export interface IEstate {
-  id: string;
-  name: string;
-  type: string;
-  status: string;
-  floorsNum: number;
-  unitsPerFloor: number;
-  details?: string;
-  createdAt: string;
+  id: string
+  name: string
+  type: string
+  status: string
+  floorsNum: number
+  unitsPerFloor: number
+  details?: string
+  createdAt: string
 }
 
 export interface IUnit {
-  id: string;
-  code: string;
-  type: string;
-  status: string;
-  details?: string;
+  id: string
+  code: string
+  type: string
+  status: string
+  details?: string
+}
+
+export interface IContract {
+  id: string
+  type: string
+  status: string
+  validity: string
+  date: string
+  details?: string
 }
 
 export interface IWallet {
-  id: string;
-  totalRecieved: number;
-  recievedOffline: number;
-  recievedOnline: number;
-  totalPending: number;
-  details?: string;
+  id: string
+  totalRecieved: number
+  recievedOffline: number
+  recievedOnline: number
+  totalPending: number
+  details?: string
 }
