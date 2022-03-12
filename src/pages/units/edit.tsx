9f30ef5bd079ba24b66eb45397f310357810d0e2
akilany <ammyass2@gmail.com>
2022-03-12 23:09:59 +0200

    revert update inputs

--- src/pages/units/edit.tsx
+++ src/pages/units/edit.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useTranslate, IResourceComponentsProps } from "@pankod/refine-core";
+import { useState } from 'react'
+import { useTranslate, IResourceComponentsProps } from '@pankod/refine-core'
 import {
   Edit,
   Form,
@@ -7,20 +7,20 @@ import {
   Select,
   useForm,
   useSelect,
-} from "@pankod/refine-antd";
-import ReactMarkdown from "react-markdown";
-import ReactMde from "react-mde";
+} from '@pankod/refine-antd'
+import ReactMarkdown from 'react-markdown'
+import ReactMde from 'react-mde'
 
-import "react-mde/lib/styles/css/react-mde-all.css";
+import 'react-mde/lib/styles/css/react-mde-all.css'
 
-import { IRenter } from "interfaces";
+import { IRenter } from 'interfaces'
 
 export const UnitEdit: React.FC<IResourceComponentsProps> = () => {
-  const [selectedTab, setSelectedTab] = useState<"write" | "preview">("write");
+  const [selectedTab, setSelectedTab] = useState<'write' | 'preview'>('write')
 
-  const t = useTranslate();
+  const t = useTranslate()
 
-  const { formProps, saveButtonProps, queryResult } = useForm<IRenter>();
+  const { formProps, saveButtonProps, queryResult } = useForm<IRenter>()
 
   // const { selectProps: categorySelectProps } = useSelect<IRenter>({
   //   resource: "categories",
@@ -31,8 +31,8 @@ export const UnitEdit: React.FC<IResourceComponentsProps> = () => {
     <Edit saveButtonProps={saveButtonProps}>
       <Form {...formProps} layout="vertical">
         <Form.Item
-          label="Code"
-          name="code"
+          label="Name"
+          name="name"
           rules={[
             {
               required: true,
@@ -42,8 +42,19 @@ export const UnitEdit: React.FC<IResourceComponentsProps> = () => {
           <Input />
         </Form.Item>
         <Form.Item
-          label="Type"
-          name="type"
+          label="Phone"
+          name="phone"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Estate"
+          name="estate"
           rules={[
             {
               required: true,
@@ -53,37 +64,37 @@ export const UnitEdit: React.FC<IResourceComponentsProps> = () => {
           <Select
             options={[
               {
-                label: "Active",
-                value: "active",
+                label: 'Active',
+                value: 'active',
               },
               {
-                label: "Draft",
-                value: "draft",
+                label: 'Draft',
+                value: 'draft',
               },
             ]}
           />
         </Form.Item>
         <Form.Item
-          label="Status"
-          name="status"
+          label="Floor"
+          name="floor"
           rules={[
             {
               required: true,
             },
           ]}
         >
-          <Select
-            options={[
-              {
-                label: "Active",
-                value: "active",
-              },
-              {
-                label: "Draft",
-                value: "draft",
-              },
-            ]}
-          />
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Number"
+          name="number"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
         </Form.Item>
 
         {/* <Form.Item
@@ -97,16 +108,7 @@ export const UnitEdit: React.FC<IResourceComponentsProps> = () => {
         >
           <Select {...categorySelectProps} />
         </Form.Item> */}
-        <Form.Item label="Details" name="details">
-          <ReactMde
-            selectedTab={selectedTab}
-            onTabChange={setSelectedTab}
-            generateMarkdownPreview={(markdown) =>
-              Promise.resolve(<ReactMarkdown>{markdown}</ReactMarkdown>)
-            }
-          />
-        </Form.Item>
       </Form>
     </Edit>
-  );
-};
+  )
+}
