828dd06aeae8118b8a5170de689475a7bf52b57a
akilany <ammyass2@gmail.com>
2022-03-15 20:33:29 +0200

    Added home sections

---
+++ src/sections/home/index.ts
@@ -0,0 +1,7 @@
+export { About } from './About'
+export { Services } from './Services'
+export { Hero } from './Hero'
+export { FAQ } from './FAQ'
+export { Team } from './Team'
+export { Contact } from './Contact'
+export { Pricing } from './Pricing'
