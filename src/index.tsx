937e53f18e3e17f15c720fc759e3898cb69a8096
ahmed atta <ahmedatta3322@gmail.com>
2022-03-16 00:14:16 +0200

    fix redirecting

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
-import "./i18n";
+import reportWebVitals from './reportWebVitals'
+import App from './App'
+import './i18n'
 
 import axios from 'axios'
 axios.defaults.headers.common = {
-    Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-  }
+  Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+}
 ReactDOM.render(
   <React.StrictMode>
     <React.Suspense fallback="loading">
-    
-        <App />
-     
+      <App />
     </React.Suspense>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
