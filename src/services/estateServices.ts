e3ebf540ad2109836a36cf623dd609c075216619
akilany <ammyass2@gmail.com>
2022-03-26 16:21:03 +0200

    added estate types endpoint

--- src/services/estateServices.ts
+++ src/services/estateServices.ts
@@ -1,10 +1,13 @@
 import { api, config } from '../utils/apiHandler'
 
-const apiClient = api('estates')
+const apiClient = api('/')
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getAll() {
-    return apiClient.get('')
+    return apiClient.get('estates')
+  },
+  getEstateTypes() {
+    return apiClient.get('estatetypes')
   },
 }
